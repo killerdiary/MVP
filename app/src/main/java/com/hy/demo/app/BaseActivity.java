577b8a70ae205fd1496ba154b360410c39035d30
rh_heyan <624867427@qq.com>
2019-09-23 10:58:15 +0800

    app

---
+++ app/src/main/java/com/hy/demo/app/BaseActivity.java
@@ -0,0 +1,32 @@
+package com.hy.demo.app;
+
+import android.content.pm.ActivityInfo;
+
+/**
+ * title 无
+ * author heyan
+ * time 19-7-11 下午2:30
+ * desc 无
+ */
+public abstract class BaseActivity extends com.hy.frame.ui.simple.BaseActivity {
+    @Override
+    public boolean isSingleLayout() {
+        return false;
+    }
+
+    @Override
+    public int getScreenOrientation() {
+        return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+    }
+
+    protected void initHeader(int drawLeft, int titleId, int strRightId) {
+        getTemplateController().setHeaderLeft(drawLeft);
+        getTemplateController().setTitle(titleId);
+        getTemplateController().setHeaderRightTxt(getString(strRightId));
+    }
+
+    protected void initHeader(int drawLeft, int titleId) {
+        getTemplateController().setHeaderLeft(drawLeft);
+        getTemplateController().setTitle(titleId);
+    }
+}
